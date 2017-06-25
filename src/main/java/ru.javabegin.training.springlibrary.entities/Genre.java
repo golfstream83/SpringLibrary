94e1c540df906a4c995c2f4c744339c742690ca1
golfstream83 <victul83@gmail.com>
2017-06-25 23:02:16 +0300

    [lesson 6] Changed database settings

--- src/main/java/ru.javabegin.training.springlibrary.entities/Genre.java
+++ src/main/java/ru.javabegin.training.springlibrary.entities/Genre.java
@@ -8,7 +8,6 @@ package ru.javabegin.training.springlibrary.entities;
 public class Genre {
     private long id;
     private String name;
-    private Long parent;
 
     public long getId() {
         return id;
@@ -26,14 +25,6 @@ public class Genre {
         this.name = name;
     }
 
-    public Long getParent() {
-        return parent;
-    }
-
-    public void setParent(Long parent) {
-        this.parent = parent;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -43,7 +34,6 @@ public class Genre {
 
         if (id != genre.id) return false;
         if (name != null ? !name.equals(genre.name) : genre.name != null) return false;
-        if (parent != null ? !parent.equals(genre.parent) : genre.parent != null) return false;
 
         return true;
     }
@@ -52,7 +42,6 @@ public class Genre {
     public int hashCode() {
         int result = (int) (id ^ (id >>> 32));
         result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (parent != null ? parent.hashCode() : 0);
         return result;
     }
 }

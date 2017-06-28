783caa6a94234dc358538f7f3decafcd5688d5c6
golfstream83 <victul83@gmail.com>
2017-06-28 23:28:13 +0300

    [lesson 13] Added search criteria to the drop-down menu

---
+++ src/main/java/ru/javabegin/training/springlibrary/enums/SearchType.java
@@ -0,0 +1,7 @@
+package ru.javabegin.training.springlibrary.enums;
+
+public enum SearchType {
+
+    AUTHOR,
+    TITLE
+}
--- src/main/java/ru/javabegin/training/springlibrary/objects/Utils.java
+++ src/main/java/ru/javabegin/training/springlibrary/objects/Utils.java
@@ -1,16 +1,48 @@
 package ru.javabegin.training.springlibrary.objects;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
+import ru.javabegin.training.springlibrary.enums.SearchType;
+
+import javax.faces.context.FacesContext;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class Utils {
 
+    private Map<String, SearchType> searchTypeList = new HashMap<String, SearchType>();
+    private SearchType selectedSearchType = SearchType.TITLE;// значение по-умолчанию
+
+
+    @Autowired
+    private MessageSource msg;
+
     private Character[] letters = new Character[]{'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М',
-                        'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'};
+            'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'};
 
     public Character[] getLetters() {
         return letters;
     }
+
+    public Map<String, SearchType> getSearchTypeList() {
+        searchTypeList.clear();
+        searchTypeList.put(msg.getMessage("author_name",
+                    null, FacesContext.getCurrentInstance().getViewRoot().getLocale()), SearchType.AUTHOR);
+        searchTypeList.put(msg.getMessage("book_name",
+                    null, FacesContext.getCurrentInstance().getViewRoot().getLocale()), SearchType.TITLE);
+        return searchTypeList;
+    }
+
+    public SearchType getSelectedSearchType() {
+        return selectedSearchType;
+    }
+
+    public void setSearchTypeList(Map<String, SearchType> searchTypeList) {
+        this.searchTypeList = searchTypeList;
+    }
+
 
 }
